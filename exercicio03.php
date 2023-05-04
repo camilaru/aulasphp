2ba12ed8efd534aaf5b009e72482f1c2013ec82a
Camila Laruccia <camilalaruccia@gmail.com>
2023-05-04 17:33:07 -0300

    exercicio3

--- exercicio03.php
+++ exercicio03.php
@@ -1,30 +1,33 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exercicio 03</title>
 </head>
+
 <body>
     <h1>Exercício 3</h1>
     <hr>
     <?php
     $salario = 2000;
-    if($salario<1500){
-    $novosalario = $salario * 1.15;
+    if ($salario < 1500) {
+        $novosalario = $salario * 1.15;
     ?>
-     <p>Sálario antigo: <?=$salario?>| Sálario atual: <?=$novosalario?> </p>
+        <p>Sálario antigo: <?= $salario ?>| Sálario atual: <?= $novosalario ?> </p>
     <?php
-    }elseif ($salario<=3000) {
-    $novosalario = $salario *1.10; 
+    } elseif ($salario <= 3000) {
+        $novosalario = $salario * 1.10;
     ?>
-    <p>Sálario antigo: <?=$salario?>| Sálario atual: <?=$novosalario?> </p>
+        <p>Sálario antigo: <?= $salario ?>| Sálario atual: <?= $novosalario ?> </p>
     <?php
-    }else {
-    $novosalario = $salario *1.05;
+    } else {
+        $novosalario = $salario * 1.05;
     }
     ?>
 
 </body>
+
 </html>
