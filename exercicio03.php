3383a3bb55ac3b5cb0a8a6bf0376accc50d45389
Camila Laruccia <camilalaruccia@gmail.com>
2023-05-04 17:27:20 -0300

    exercicio condicionais

--- exercicio03.php
+++ exercicio03.php
@@ -7,6 +7,24 @@
     <title>Exercicio 03</title>
 </head>
 <body>
-    <h1></h1>
+    <h1>Exercício 3</h1>
+    <hr>
+    <?php
+    $salario = 2000;
+    if($salario<1500){
+    $novosalario = $salario * 1.15;
+    ?>
+     <p>Sálario antigo: <?=$salario?>| Sálario atual: <?=$novosalario?> </p>
+    <?php
+    }elseif ($salario<=3000) {
+    $novosalario = $salario *1.10; 
+    ?>
+    <p>Sálario antigo: <?=$salario?>| Sálario atual: <?=$novosalario?> </p>
+    <?php
+    }else {
+    $novosalario = $salario *1.05;
+    }
+    ?>
+
 </body>
 </html>
